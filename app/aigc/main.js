66fae97d3deb6e7a948f981008adf1d43e8e4553
fefeding <fefeding@jintengtech.com>
2024-03-21 16:53:25 +0800

    :art: about me

---
+++ app/aigc/main.js
@@ -0,0 +1,11 @@
+[
+    { title: 'AIGC导航',
+    modal: false,    
+    position: 'center',   //显示位置,top=顶部,left =左边,center居中       
+    url: 'https://www.jm47.com/', 
+    width: 800,
+    height: 600,
+    resizeable: true,    
+    des: true
+}
+];
