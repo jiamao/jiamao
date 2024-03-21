03c0335304f3f32a856a08024d0bde427c966204
fefeding <fefeding@jintengtech.com>
2024-03-21 16:44:42 +0800

    :art: about me

---
+++ app/about/main.js
@@ -0,0 +1,11 @@
+[
+    { title: '关于我',
+    modal: false,    
+    position: 'center',   //显示位置,top=顶部,left =左边,center居中        
+    width: 600,
+    height: 480,
+    resizeable: true,    
+    des: false,
+    bgMenu: true//加到桌面右健菜单中
+}
+];
