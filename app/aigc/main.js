f65b966ce57535107872ac288e958b7fc84a08ba
fefeding <fefeding@jintengtech.com>
2024-03-22 09:59:26 +0800

    :art: about me

--- app/aigc/main.js
+++ app/aigc/main.js
@@ -4,7 +4,7 @@
     position: 'center',   //显示位置,top=顶部,left =左边,center居中       
     url: 'https://www.jm47.com/', 
     iframe: true,
-    ico: "~/app/jm/py.png",
+    ico: "https://jtcospublic.ciccten.com/config-server/1709623426067209060/fefeding%20%281%29.png",
     width: 800,
     height: 600,
     state:$jm.winState.max,
