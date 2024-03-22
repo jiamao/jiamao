01495a67dbb7cc8d86cac01ea9c0d7225abb9129
fefeding <fefeding@jintengtech.com>
2024-03-22 10:06:28 +0800

    :art: about me

--- app/about/main.js
+++ app/about/main.js
@@ -1,11 +1,13 @@
 [
-    { title: '关于我',
-    modal: false,    
-    position: 'center',   //显示位置,top=顶部,left =左边,center居中        
-    width: 600,
-    height: 480,
-    resizeable: true,    
-    des: true,
-    bgMenu: true//加到桌面右健菜单中
-}
+    { 
+        title: '关于我',
+        ico: "https://jtcospublic.ciccten.com/config-server/1711073103519927178/10080714.png",
+        modal: false,    
+        position: 'center',   //显示位置,top=顶部,left =左边,center居中        
+        width: 600,
+        height: 480,
+        resizeable: true,    
+        des: true,
+        bgMenu: true//加到桌面右健菜单中
+    }
 ];
