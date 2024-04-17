36fb8dde7040d496db407ffea77bf3b036054aeb
fefeding <fefeding@jintengtech.com>
2024-04-17 10:24:34 +0800

    :art: footer

--- app/aigc/main.js
+++ app/aigc/main.js
@@ -2,7 +2,7 @@
     { title: 'AIGC导航',
     modal: false,    
     position: 'center',   //显示位置,top=顶部,left =左边,center居中       
-    url: 'https://www.jm47.com/', 
+    url: 'https://www.aigcwhere.com/', 
     iframe: true,
     ico: "https://jtcospublic.ciccten.com/config-server/1709623426067209060/fefeding%20%281%29.png",
     width: 800,
